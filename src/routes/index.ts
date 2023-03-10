86f323c92e3341ad5283a789668c81fdc79fddb4
MatGod135 <matheuslealcosta@icloud.com>
2023-03-10 16:31:04 -0300

    cria rota de login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 const express = require('express');
-
+import { RestauranteController } from "../controllers/RestauranteController";
+import { ProdutoController } from "../controllers/ProdutosController";
+import LoginController from "../controllers/LoginController";
+import { PedidosController } from "../controllers/PedidosController";
 
 const routes = express.Router();
+
+routes.post('/login', LoginController.login);
+
+
 export { routes };
