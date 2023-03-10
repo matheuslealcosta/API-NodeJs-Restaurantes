aa08d54bf65e62cbd969a45c87b575986386a1a8
MatGod135 <matheuslealcosta@icloud.com>
2023-03-10 15:56:12 -0300

    Function getProdutosById  ProdutosController

--- src/controllers/ProdutosController.ts
+++ src/controllers/ProdutosController.ts
@@ -66,5 +66,28 @@ export class ProdutoController {
 
 
 
+  static getProdutoById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const produto = await prisma.produto.findUnique({
+        where: { id: parseInt(id) }
+      });
+
+      if (!produto) {
+        res.status(404).json({ message: `Produto com ID ${id} não encontrado.` });
+      } else {
+        res.status(200).json(produto);
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
+
+
+
+
 
 }
